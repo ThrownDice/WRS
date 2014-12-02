a532a4e9cd5b394f96fecc7afca03e901380911a
lana123 <darkwhddl8@naver.com>
2014-12-03 08:07:57 +0900

    141203 frontend_lee
    1. moved timer function to new file "common-1.js"
    2. added script src(/common/js/common-1.js) for admin/reserve page
    3. added menu_submenu/table_submenu division for reserve page which will
    express menulists and tablelists by clicking "menu" or "table".

--- web/admin/js/admin.js
+++ web/admin/js/admin.js
@@ -2,32 +2,3 @@
  * Created by TD on 2014-11-16.
  */
 
-//changes the 'time' expressed top of the screen.
-function timer() {
-    var theDate = new Date();
-    var week=["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
-    var yr=theDate.getFullYear();
-    var mnth=theDate.getMonth()+1;
-    var today=theDate.getDate();
-    var hr=theDate.getHours();
-    var min=theDate.getMinutes();
-    var dowk=week[theDate.getDay()];
-    
-   //if the value is smaller than 10, shows '0x' form.
-    mnth=addZeroToTimer(mnth);
-    today=addZeroToTimer(today);
-    hr=addZeroToTimer(hr);
-    min=addZeroToTimer(min);
-    
-    document.getElementById("timer").innerHTML = yr +" "+mnth+" "+today+" "+hr+":"+min+" "+dowk;
-}
-
-/*timer changes every 1/60 minute. for accuracy.
-¡ØThere is a problem that because of this interval,
-timer does not appear immediately when page is loaded.*/
-var itvForTime = setInterval(function(){timer()},1000);
-
-function addZeroToTimer(i){
-	if (i<10) {i="0"+i};
-	return i;
-}
