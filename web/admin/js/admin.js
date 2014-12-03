97491ad0e3451b9325f6cf3d53071c10f28b84bd
lana123 <darkwhddl8@naver.com>
2014-12-03 23:57:42 +0900

    141203 frontend_admin_lee
    c&p ajax function

--- web/admin/js/admin.js
+++ web/admin/js/admin.js
@@ -2,3 +2,20 @@
  * Created by TD on 2014-11-16.
  */
 
+function loadMenu(){
+	$.ajax({"/action/get_menu"}
+		.done(function(response){
+			var result=JSON.parse(response);
+			var menuData=result.menu;
+			var len=menuData.length;
+		
+			for(var i=0, i<len, i++){
+				var newData=$(".tab_menu_table").append(<tr>);
+				newData.append($("<img src=/menu/"+menuData[i].price+"¿ø"+"</span>"));
+			}
+		});
+}
+
+function loadTable(){
+	
+}
