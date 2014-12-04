3b8da113e87f9763b36488206addb36cb72c21ad
lana123 <darkwhddl8@naver.com>
2014-12-04 23:41:22 +0900

    141204 frontend_admin_lee

--- web/admin/js/admin.js
+++ web/admin/js/admin.js
@@ -2,6 +2,18 @@
  * Created by TD on 2014-11-16.
  */
 
+function loadRState(){
+	$.ajax({"/action/get_reservation"}
+		.done(function(response){
+			var result=JSON.parse(response);
+			var menuData=result.menu;
+			var len=menuData.length;
+
+			for(var i=0, i<len, i++){
+			}
+	});
+}
+
 function loadMenu(){
 	$.ajax({"/action/get_menu"}
 		.done(function(response){
@@ -11,11 +23,41 @@ function loadMenu(){
 		
 			for(var i=0, i<len, i++){
 				var newData=$(".tab_menu_table").append(<tr>);
-				newData.append($("<img src=/menu/"+menuData[i].price+"¿ø"+"</span>"));
+				newData.append($("<td><img src=/menu/"+"</td><td>"+menuData[i].price+"¿ø"+"</td><td>"+"</span></td>"));
 			}
 		});
+	)
 }
 
 function loadTable(){
+	$.ajax({"/action/get_table"}
+	.done(function(response){
+		var result=JSON.parse(response);
+		var menuData=result.menu;
+		var len=menuData.length;
 	
+		for(var i=0, i<len, i++){}
+)};
 }
+
+//practice
+$(document).ready(function(){
+  $(function(){
+    $(".contents").show();
+    $(".tab_menu").css("display","none");
+  });
+  $("#menu_reserve").click(function(){
+	$(".tab_menu").css("display","none");
+	$(".reservation").css("display","inline");
+  });
+  $("#menu_table").click(function(){
+	$(".reservation").css("display","none");
+	$(".menu tab_menu").css("display","none");
+	$(".table tab_menu").css("display","inline");
+  });
+  $("#menu_menu").click(function(){
+	$(".reservation").css("display","none");
+	$(".table tab_menu").css("display","none");
+	$(".menu tab_menu").css("display","inline");
+  });
+});
