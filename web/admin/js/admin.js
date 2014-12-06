592f749a40a54dbeb253568c00196a6bfdf6442d
lana123 <darkwhddl8@naver.com>
2014-12-06 10:44:22 +0900

    141206 frontend_admin_lee
    not finished

--- web/admin/js/admin.js
+++ web/admin/js/admin.js
@@ -7,13 +7,39 @@ $(document).ready(function(){
 	  $.ajax({"/action/get_reservation"}
 		.done(function(response){
 			var result=JSON.parse(response);
-			var menuData=result.menu;
-			var len=menuData.length;
+			var reservationData=result.reservation;
+			var len=result.length;
 
 			for(var i=0, i<len, i++){
+				var newData=$(".reservation_table").append(<tr>);
+				newData.append($("<td>"+reservationData[i].reserveNum+"</td><td>"+reservationData[i].reserveTime+"</td><td>"+reservationData[i].name+"</td><td>"+reservationData[i].phone+"</td><td class='reserved_menu'>"+"메뉴"+"</td><td class='reserved_table'>"+"좌석"+"</td><td>"+"취소"+"</td>"))
 			}
 		});
 	);
+		$.ajax({"/action/get_menu"}
+		.done(function(response){
+			var result=JSON.parse(response);
+			var menuData=result.menu;
+			var len=menuData.length;
+		
+			for(var i=0, i<len, i++){
+				var newData=$(".tab_menu_table").append(<tr>);
+				newData.append($("<td>"+menuData[i].id+"</td><td>"+menuData[i].name+"</td><td>"+menuData[i].price+"원</td><td><span><img src=/menu/"+menuData[i].img+"></span></td>"));
+			}
+		});
+	)
+		$.ajax({"/action/get_table"}
+		.done(function(response){
+			var result=JSON.parse(response);
+			var tableData=result.table;
+			var len=menuData.length;
+
+			for(var i=0, i<len, i++){
+				var newData=$(".table_info_table").append(<tr>);
+				newData.append($("<td>"+tableData[i].id+"</td><td>"+tableData[i].capacity+"</td><td>"+tableData[i].available+"</span></td>"))
+			}
+		});
+	)
     $(".contents").show();
     $("#table_tabmn").hide();
     $("#menu_tabmn").hide();
